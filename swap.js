4b790b771321d057542cddec7a85cadf1a6924df
lavanyasb1619 <lavanyasb1619@gmail.com>
2026-03-10 18:34:08 +0530

    swap added

---
+++ swap.js
@@ -0,0 +1,25 @@
+// Swap by third variable 
+
+let a = 10; 
+let b = 20; 
+console.log(a,b); 
+let temp =0; 
+temp = a; 
+a=b
+b=temp; 
+console.log(a,b); 
+//Swap without third variable 
+let c=40; 
+let d=50; 
+console.log(c,d); 
+[c,d] = [d,c]; 
+console.log(c,d); 
+
+// Arthematic way 
+let e=10; 
+let f=20; 
+console.log(e,f); 
+e= e+f; 
+f= e-f; 
+e= e-f 
+console.log(e,f); 
